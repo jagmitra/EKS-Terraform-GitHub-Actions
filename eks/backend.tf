3020212d2aa24375aa9469b03bc3c9997a8e60de
Gaurav Sharma <jagmitra@hotmail.com>
2025-04-10 16:46:57 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,22 +1,22 @@
 terraform {
   required_version = ">= 1.9.3, <= 1.11.4"
-}
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = ">= 2.7.0"
     }
   }
+
   backend "s3" {
-    bucket         = "dev-tf-bucket"
+    bucket         = "bkd24nov23"
     region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
+    key            = "terraform/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region  = var.aws-region
+  region = "us-east-1"
 }
